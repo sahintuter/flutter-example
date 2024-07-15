0504405d2d0d3fb1b1ec8b8cf61d77fc52d92401
Şahin Tüter <sahintuter000@gmail.com>
2024-07-15 10:53:22 +0300

    weather project created

---
+++ simple_weather_app/lib/pages/HomePage/view/home_page_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
